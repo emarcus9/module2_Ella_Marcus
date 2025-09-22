fd88a6418525600dccb2f4264700b9efabfd2c95
emarcus9 <emarcus9@student.touro.edu>
2025-09-21 22:57:24 -0400

    Add files via upload

---
+++ Campus_Cafe_test_run.py
@@ -0,0 +1,22 @@
+Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+=============== RESTART: C:\Users\ellam\Downloads\Campus_Cafe.py ===============
+--Campus Cafe--
+Coffee: $2.25
+Muffin: $2.75
+Bagel: $2.50
+How many coffees would you like? 2
+How many muffins would you like? 3
+How many bagels would you like? 4
+Enter tip percent (ex: 10 for 10%): 10
+
+--Receipt--
+2 x Coffee @ $2.25 = $4.50
+3 x Muffin @ $2.75 = $8.25
+4 x Bagel @ $2.50 = $10.00
+ Subtotal: $22.75
+ Tax(8.875%): $2.02
+ Tip (10.0%): $2.27
+ Total: $27.04
+ Thank you for shopping with us!
